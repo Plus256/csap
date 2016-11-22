3ad8be940711ca452e13e984b8ff45e681c08bcf
Collins Wagaba <wagabaplus256@gmail.com>
2016-11-22 17:07:16 +0000

    done with footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,8 @@
         <footer>
-            
+            <div class="wrapper">
+                    <div id="copyright">Copyright &copy; <?php echo date('Y')." "; ?>Brian and Collins</div>
+                    <div class="spacer"></div>
+            </div>
         </footer>
     </body>
 </html>
