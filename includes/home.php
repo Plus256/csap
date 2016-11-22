58b22e9cb649b29129812cccd4c791a04a7525f0
Collins Wagaba <wagabaplus256@gmail.com>
2016-11-22 18:02:29 +0000

    homepage done

---
+++ includes/home.php
@@ -0,0 +1,7 @@
+<section id="homepage">
+    <div class="wrapper">
+        <p id="homepage_intro">You'll need to sign in with your Twitter credentials to Enter the Chatroom.</p>
+        <a id="homepage_signin_button" href="#">Enter Chatroom</a>
+        <div class="spacer"></div>
+    </div>
+</section>
