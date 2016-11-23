ae063d4ea26a2da8d58305ea50d269d108fb49d9
Collins Wagaba <wagabaplus256@gmail.com>
2016-11-23 16:11:02 +0000

    fetched user data

--- includes/user.php
+++ includes/user.php
@@ -2,5 +2,9 @@
 require_once("includes/db.php");
 //get user data from DB
 $stmt=$conn->prepare("SELECT * FROM user WHERE id=?");
-$stmt->execute([$user_id]);
+$stmt->execute([$user_id]);//$user_id is taken from wrapper that extracts ID from session
+$user=$stmt->fetch();//default fetch is an associative array - was set at connection time
+$user_name=$user["name"];
+$user_screen_name=$user["screenname"];
+$user_profile_image_url=$user["profileimageurl"];
 ?>
