f1b8a8b7c2725002cc2021d782a94d4ff356f5a4
Collins Wagaba <wagabaplus256@gmail.com>
2016-11-23 13:19:05 +0000

    add user credentials to DB

--- twitter_oauth_callback.php
+++ twitter_oauth_callback.php
@@ -16,8 +16,36 @@ else{
     $connection=new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
     $access_token=$connection->oauth("oauth/access_token", ["oauth_verifier"=>$_REQUEST["oauth_verifier"]]);
     
-    //grant user access with a session based on the access token
-    $_SESSION["logged"]=$access_token;
-    header('Location: ./');
+    //the point at which user data is added to DB
+    require_once("includes/db.php");
+    
+    //we need to check if user data already exists with reference to the generated ACCESS TOKEN
+    $user_access_token=$access_token['oauth_token'];
+    $stmt=$conn->prepare("SELECT 1 FROM user WHERE token=?");//returns True or False when executed
+    $stmt->execute([$user_access_token]);
+    $count=stmt->fetchColumn();
+    
+    //User Exists
+    if($count==1){
+        //grant user access with a session based on the access token
+        $_SESSION["logged"]=$access_token;//our UNIQUE IDENTIFIER
+        header('Location: ./');
+    }
+    else{//New User
+        $user=$connection->get("account/verify_credentials");
+        $user_name=$user->name;
+        $user_screen_name=$user->screen_name;
+        $user_profile_image_url=$user->profile_image_url;
+        $user_access_token_secret=$access_token['oauth_token_secret'];
+        //add data to DB
+        $stmt=$conn->prepare("INSERT INTO user(token, tokensecret, name, screenname, profile_image_url) VALUES (:token, :token_secret, :name, :screen_name, :profile_image_url)");
+        $stmt->execute(["token"=>$user_access_token, "token_secret"=>$user_access_token_secret, "name"=>$user_name, "screen_name"=>$user_screen_name, "profile_image_url"=>$user_profile_image_url]);
+        $stmt->rowCount();
+        if($stmt>0){//record created
+            //grant user access with a session based on the access token
+            $_SESSION["logged"]=$access_token;//our UNIQUE IDENTIFIER
+            header('Location: ./');
+        }
+    }
 }
 ?>
