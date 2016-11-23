b339a7b72a6b922bdc0fe55ef9e0d05b5c71bede
Collins Wagaba <wagabaplus256@gmail.com>
2016-11-23 07:47:44 +0000

    implementing twitter oauth sign in

---
+++ twitter_oauth_callback.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require_once("twitter_oauth_credentials.php");
+
+require("twitteroauth/autoload.php");
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$request_token=[];
+$request_token['oauth_token']=$_SESSION["oauth_token"];
+$request_token['oauth_token_secret']=$_SESSION["oauth_token_secret"];
+
+if (isset($_REQUEST["oauth_token"]) && $request_token['oauth_token'] !== $_REQUEST["oauth_token"]) {
+    // Abort! Something is wrong.
+}
+else{
+    $connection=new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
+    $access_token=$connection->oauth("oauth/access_token", ["oauth_verifier"=>$_REQUEST["oauth_verifier"]]);
+    
+    //store the values below in DB
+    $twitter_access_token=$access_token['oauth_token'];
+    $twitter_access_token_secret=$access_token['oauth_token_secret'];
+    
+    //then grant user access with a session carrying the user ID
+    $_SESSION["logged"];
+    header('Location: index.php');
+}
+?>
---
+++ twitter_oauth_credentials.php
@@ -0,0 +1,5 @@
+<?php
+define("CONSUMER_KEY", "xnFGEDJS7k1hPWl5tHhHeb8J9");
+define("CONSUMER_SECRET", "UK0w2WEzLzZu1A9Mv3ePGMHH1GxAqm0Ri8MnO1Jp00u5nX9M7u");
+define("OAUTH_CALLBACK", "http://plus256.com/csap/twitter_oauth_callback.php");
+?>
