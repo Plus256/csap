a926430985bd9a169eb9dc32824fb753bad7d3af
Collins Wagaba <wagabaplus256@gmail.com>
2016-11-23 09:07:43 +0000

    cleaning redirect url

--- includes/logout.php
+++ includes/logout.php
@@ -3,6 +3,6 @@
 session_start();
 session_unset();//remove all session variables
 if(session_destroy(){//destroy the session
-    header('Location: ../index.php');//return to home page
+    header('Location: ../');//return to home page
 }
 ?>
