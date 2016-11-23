ede73a1c2aa25b5507438cb4c9e7b646708590ef
Collins Wagaba <wagabaplus256@gmail.com>
2016-11-23 10:18:48 +0000

    corrected misbehaving logout script

--- includes/logout.php
+++ includes/logout.php
@@ -2,7 +2,7 @@
 //Logout User
 session_start();
 session_unset();//remove all session variables
-if(session_destroy(){//destroy the session
+if(session_destroy()){//destroy the session
     header('Location: ../');//return to home page
 }
 ?>
