98b5d26cc4056b7c85ac84cb6533509e54184be9
Collins Wagaba <wagabaplus256@gmail.com>
2016-11-24 10:49:09 +0000

    preparing user dhashboard

---
+++ includes/dashboard.php
@@ -0,0 +1,6 @@
+<?php
+require_once('includes/user.php');//fetch all user data from DB
+?>
+<section id="dashboard">
+    
+</section>
--- includes/main.php
+++ includes/main.php
@@ -2,7 +2,7 @@
     <?php
     if(isset($_SESSION["logged"])){//user logged in. display user dashboard with chats and more
         $user_id=$_SESSION["logged"];//extract user ID from session variable
-        require_once('includes/user.php');//include user information and more
+        require_once('includes/dashboard.php');//include user dashboard
     }
     else{//display homepage with link to login
         require_once('includes/home.php');
