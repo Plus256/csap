c1ea14fffa1f379890cea61e97a106fe0df250b4
Collins Wagaba <wagabaplus256@gmail.com>
2016-11-30 13:32:59 +0000

    fine tuning homepage layout. the paddings and margins were ruining it :D

--- includes/home.php
+++ includes/home.php
@@ -1,10 +1,7 @@
 <section id="homepage">
-    <div id="homepage_hero"><!--image called as bg. looking for flexbox overflow:hidden solution--></div>
-    <div id="homepage_action">
-        <div class="wrapper">
-            <p id="homepage_intro">You'll need to sign in with your Twitter credentials to Enter the Chatroom.</p>
-            <a id="homepage_signin_button" href="twitter_oauth.php">Enter Chatroom</a>
-            <div class="spacer"></div>
-        </div>
+    <div class="wrapper">
+        <p id="homepage_intro">You'll need to sign in with your Twitter credentials to Enter the Chatroom.</p>
+        <a id="homepage_signin_button" href="twitter_oauth.php">Enter Chatroom</a>
+        <div class="spacer"></div>
     </div>
 </section>
