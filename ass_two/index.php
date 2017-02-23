fba8c28916478819a1380c6c701f4d861388b236
Collins Wagaba <wagabaplus256@gmail.com>
2017-02-23 09:53:16 +0000

    Initializing Excel File Read Project

---
+++ ass_two/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta name="description" content="Excel File Read">
+    <title>Excel File Read</title>
+</head>
+<body>
+    <?php require_once("file_read.php"); ?>
+</body>
+</html>
